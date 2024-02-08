1e628e06b7f09e4684596a79b9cab8ddf77ed0b2
xiaonanpols21 <xiaonanpolspols@gmail.com>
2024-02-08 10:34:03 +0100

    data in html weergeven

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,30 @@
+const main = document.querySelector(".personalInfo");
+
 async function siteInfo() { //Functie die de JSON file gaat ophalen en waardes gaat veranderen
     const response = await fetch("./public/data/siteData.json"); //Maakt de variabele response aan door het JSON bestand te fetchen (en te wachten tot het binnen is doormiddel van await)
     const siteJson = await response.json(); //Zet het JSON bestand wat binnen komt als text om naar een JSON
     // document.getElementById('namePlaceholder').innerText = siteJson.name; //Zoekt het element met de Id namePlaceholder op en past de waarde aan gebaseerd op wat er in de variabele siteJson staat met de key (idk of het ook zo heet in JSON) name
     console.log(siteJson)
+    changeData(siteJson);
 }
-
 siteInfo() //Voert de functie uit 
+
+function changeData(siteJson) {
+    Object.values(siteJson.mensen).forEach(item => {
+        const name = item.naam;
+
+        const html = 
+        `
+        <article>
+            <h2>${name}</h2>
+            <ul>
+                <li>Day of birth</li>
+                <li>City</li>
+                <li>Favorite music genre</li>
+            </ul>
+            <img src="" alt="Profile picture">
+        </article>
+        `;
+        main.insertAdjacentHTML("beforeend", html);
+    });
+}
