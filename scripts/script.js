eba90ddfefdf578d8a34008a3e207caeb9a08025
xiaonanpols21 <xiaonanpolspols@gmail.com>
2024-02-12 14:12:30 +0100

    move json file

--- scripts/script.js
+++ scripts/script.js
@@ -15,7 +15,7 @@ function hideLoading() {
 }
 
 async function siteInfo() { //Functie die de JSON file gaat ophalen en waardes gaat veranderen
-    const response = await fetch("./public/data/siteData.json"); //Maakt de variabele response aan door het JSON bestand te fetchen (en te wachten tot het binnen is doormiddel van await)
+    const response = await fetch("info.json"); //Maakt de variabele response aan door het JSON bestand te fetchen (en te wachten tot het binnen is doormiddel van await)
     const siteJson = await response.json(); //Zet het JSON bestand wat binnen komt als text om naar een JSON
     // document.getElementById('namePlaceholder').innerText = siteJson.name; //Zoekt het element met de Id namePlaceholder op en past de waarde aan gebaseerd op wat er in de variabele siteJson staat met de key (idk of het ook zo heet in JSON) name
     // console.log(siteJson)
